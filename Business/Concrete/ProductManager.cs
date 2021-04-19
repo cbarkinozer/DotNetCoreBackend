4b8d333f288971f0ab2f5e0bf371895c9d07262f
barkin <c.barkinozer@gmail.com>
2021-04-19 10:40:18 +0300

    Create ConsoleUI Modified IProductService and ProductManager

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -17,7 +18,17 @@ namespace Business.Concrete
 
         public List<Product> GetAll()
         {
-            return _productDal getAll();
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min&&p.UnitPrice<=max);
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -12,7 +13,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Product entity)
         {
-            using (NorthwindContext context= new NorthwindContext)
+            using (NorthwindContext context= new NorthwindContext())
             {
                 var addedEntity = context.Entry(entity);
                 addedEntity.State = EntityState.Added;
@@ -22,7 +23,7 @@ namespace DataAccess.Concrete.EntityFramework
 
         public void Delete(Product entity)
         {
-            using(NorthwindContext context = new NorthwindContext)
+            using(NorthwindContext context = new NorthwindContext())
             {
                 var deletedEntity = context.Entry(entity);
                 deletedEntity.State = EntityState.Deleted;
@@ -32,20 +33,24 @@ namespace DataAccess.Concrete.EntityFramework
 
         public Product Get(Expression<Func<Product, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext()) 
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
         }
 
         public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
         {
-            using () 
+            using (NorthwindContext context = new NorthwindContext()) 
             {
-
+                return filter == null ? context.Set<Product>().ToList() :
+                    context.Set<Product>().Where(filter).ToList();
             }
         }
 
         public void Update(Product entity)
         {
-            using (NorthwindContext context = new NorthwindContext)
+            using (NorthwindContext context = new NorthwindContext())
             {
                 var updatedEntity = context.Entry(entity);
                 updatedEntity.State = EntityState.Modified;
