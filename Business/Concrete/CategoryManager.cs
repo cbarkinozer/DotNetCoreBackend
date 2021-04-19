70cf7123defb6853f780ac4b9bb5a47293a65084
barkin <c.barkinozer@gmail.com>
2021-04-19 16:17:44 +0300

    Add DTO

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+        public List<Category> GetAll(ICategoryDal categoryDal)
+        {
+            //Bussiness codes
+            return _categoryDal.GetAll();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetById(int categoryId)
+        {
+            return _categoryDal.Get(c=>c.CategoryId==categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,14 +8,24 @@ namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
+        {
+            ProductTest();
+            CategoryManager categoryManager = new CategoryManager();
+            foreach (var category in categoryManager.GetAll()) 
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll()) 
+            foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
             }
-        
         }
     }
 }
