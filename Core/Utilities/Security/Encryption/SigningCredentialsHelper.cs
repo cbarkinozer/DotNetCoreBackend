e179e48035358b55c59027a7784b36e175d0a331
barkin <c.barkinozer@gmail.com>
2021-04-30 20:17:45 +0300

    Security Hashing

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper CreateSigningCredentials(SecurityKeyHelper securityKey) 
+    {
+        return new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha512Signature);
+    }
+    
+}
